ee772b2f12e3b8b9040ef57e37bda98500ff3012
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-01 20:03:29 +0100

    Model Movie : La anotación de datos [Column(TypeName = "decimal(18, 2)")] permite que Entity Framework Core asigne correctamente Price a la moneda en la base de datos. campo releaseDate mostrar como Release Date

--- BasicoWebRazorCore/Models/Movie.cs
+++ BasicoWebRazorCore/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,8 +13,11 @@ namespace BasicoWebRazorCore.Models
         public string Title { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
