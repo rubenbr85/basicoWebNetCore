9e5cf4c9ae39cfc75200a718e9737139835382d4
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 14:16:45 +0100

    Migracion de base de datos

--- BasicoWebRazorCore/Migrations/20200202131215_ModelGenre.Designer.cs
+++ BasicoWebRazorCore/Migrations/20200202131215_ModelGenre.Designer.cs
@@ -4,14 +4,16 @@ using BasicoWebRazorCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BasicoWebRazorCore.Migrations
 {
     [DbContext(typeof(BasicoWebRazorCoreContext))]
-    partial class BasicoWebRazorCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200202131215_ModelGenre")]
+    partial class ModelGenre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
