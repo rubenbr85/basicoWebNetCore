b3b84e33f912fd8528c918e502875bca8bef7484
Ruben Berreguero <rubenbr85@gmail.com>
2020-02-02 14:56:17 +0100

    MIgrar base de datos relacion Movie con Genre

---
+++ BasicoWebRazorCore/Migrations/20200202134258_Relacion_Movie_Genre.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BasicoWebRazorCore.Migrations
+{
+    public partial class Relacion_Movie_Genre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Genre_GenreID",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreID",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Genre_GenreID",
+                table: "Movie",
+                column: "GenreID",
+                principalTable: "Genre",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Genre_GenreID",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreID",
+                table: "Movie",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Genre_GenreID",
+                table: "Movie",
+                column: "GenreID",
+                principalTable: "Genre",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
